311bc785075a1ad71a64b2ebd340e66fd31f3276
JacopoWolf <work.jacopocomparin@gmail.com>
2020-06-12 19:13:48 +0200

    Updated TEST.SimpleStack

---
+++ tests/StackInjector.TEST.SimpleStack/Services/BadEntryPointGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StackInjector.Attributes;
+
+namespace StackInjector.TEST.SimpleStack.Services
+{
+    [Service(Pattern = Settings.InstantiationPattern.AlwaysCreate)]
+    class BadEntryPointGenerator
+    {
+        public void Test()
+        {
+            Console.WriteLine("something");
+        }
+    }
+}
