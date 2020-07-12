b3bac2233de89d1d0933338a3b4e439d724e29bc
JacopoWolf <work.jacopocomparin@gmail.com>
2020-07-12 22:25:34 +0200

    Update InjectionCore.injection.cs

--- StackInjector/Core/InjectionCore.injection.cs
+++ StackInjector/Core/InjectionCore.injection.cs
@@ -84,22 +84,21 @@ namespace StackInjector.Core
 
             foreach( var serviceProperty in properties )
             {
-                try
-                {
-                    var serviceInstance =
-                    this.InstTypeOrServiceEnum
-                    (
-                        serviceProperty.PropertyType,
-                        serviceProperty.GetCustomAttribute<ServedAttribute>(),
-                        ref instantiated
-                    );
+                
+                var serviceInstance =
+                this.InstTypeOrServiceEnum
+                (
+                    serviceProperty.PropertyType,
+                    serviceProperty.GetCustomAttribute<ServedAttribute>(),
+                    ref instantiated
+                );
 
-                    serviceProperty.SetValue(instance, serviceInstance);
-                }
-                catch( ArgumentException )
-                {
+                if (serviceProperty.GetSetMethod() is null)
                     throw new NoSetterException(type, $"Property {serviceProperty.Name} of {type.FullName} has no setter!");
-                }
+
+                serviceProperty.SetValue(instance, serviceInstance);
+                
+
 
 
             }
