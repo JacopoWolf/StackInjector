70f34b8bb9d2f1a25d678c6a21630d5d65c50cbf
Jacopo <work.jacopocomparin@gmail.com>
2021-02-20 08:43:33 +0100

    Update CommonTestingMethods.cs

--- tests/StackInjector.TEST.BlackBox/CommonTestingMethods.cs
+++ tests/StackInjector.TEST.BlackBox/CommonTestingMethods.cs
@@ -30,18 +30,18 @@ namespace StackInjector.TEST.BlackBox
 			}
 		}
 
-#if RELEASE
-		[TearDown]
-		public void ConsoleLogOnError ()
-		{
-			var result = TestContext.CurrentContext.Result.Outcome.Status;
-			if ( result == TestStatus.Failed || result == TestStatus.Inconclusive )
-			{
-				Console.WriteLine(
-					$"\t\tFailed:{TestContext.CurrentContext.Test.FullName}\n\t\tMessage:{TestContext.CurrentContext.Result.Message}"
-					);
-			}
-		}
-#endif
+////#if RELEASE
+////		[TearDown]
+////		public void ConsoleLogOnError ()
+////		{
+////			var result = TestContext.CurrentContext.Result.Outcome.Status;
+////			if ( result == TestStatus.Failed || result == TestStatus.Inconclusive )
+////			{
+////				Console.WriteLine(
+////					$"\t\tFailed:{TestContext.CurrentContext.Test.FullName}\n\t\tMessage:{TestContext.CurrentContext.Result.Message}"
+////					);
+////			}
+////		}
+////#endif
 	}
 }
